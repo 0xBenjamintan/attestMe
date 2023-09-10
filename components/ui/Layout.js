de450bc68bc2c8013fb204e4ab7a6d612f8c42ad
Benjamin <tankerkzheng1@gmail.com>
2023-09-11 02:26:17 +0800

    Update Layout.js

--- components/ui/Layout.js
+++ components/ui/Layout.js
@@ -1,13 +1,16 @@
 import Header from './Header';
 import Footer from './Footer';
+import Link from "next/link";
 
 const RootLayout = ({ children }) => {
     return (
       <div className="flex flex-col min-h-screen mx-auto max-w-6xl px-4 pt-8 pb-16">
-        <div className="flex-grow">
-          <Header />
-          <main className="my-0">{children}</main>
-        </div>
+        <Link href="/">
+          <div className="flex-grow">
+            <Header />
+            <main className="my-0">{children}</main>
+          </div>
+        </Link>
       </div>
     );
   };
