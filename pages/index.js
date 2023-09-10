f8d1ca42d29d5226414830e7e5861c018d680519
Benjamin <tankerkzheng1@gmail.com>
2023-09-10 14:22:18 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,22 +15,7 @@ export default function Home() {
           <code className="font-mono font-bold">pages/index.js</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <ConnectButton />
         </div>
       </div>
 
