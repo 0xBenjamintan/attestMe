e983bc7c30d172390772a8aee5718c44874cdfd7
Benjamin <tankerkzheng1@gmail.com>
2023-09-10 21:43:53 +0800

    feat: updated global styling

---
+++ components/ui/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <footer>
+        <p>&copy; {new Date().getFullYear().toString()} Ethereum Singapore: Team T0X1C1TY | AttestMe</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
