789e09765cfbfedadb68c07c3296634c100827b5
Benjamin <tankerkzheng1@gmail.com>
2023-09-10 22:54:30 +0800

    Update Layout.js

--- components/ui/Layout.js
+++ components/ui/Layout.js
@@ -6,7 +6,7 @@ const RootLayout = ({ children }) => {
       <div className="flex flex-col min-h-screen mx-auto max-w-6xl px-4 pt-8 pb-16">
         <div className="flex-grow">
           <Header />
-          <main className="my-0 py-16">{children}</main>
+          <main className="my-0 py-8">{children}</main>
           <Footer />
         </div>
       </div>
