30cf375ce90981d6743060bf82e023d40d18871f
Terry Lau <terrylau0708@gmail.com>
2023-09-12 02:35:43 +0800

    Update index.jsx

--- pages/dashboard/client/index.jsx
+++ pages/dashboard/client/index.jsx
@@ -85,53 +85,9 @@ function Home() {
             </div>
           </CardContent>
         </Card>
-        {/* Dialog Button */}
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button variant="outline">Create Post</Button>
-          </DialogTrigger>
-          <DialogContent className="sm:max-w-[425px]">
-            <DialogHeader>
-              <DialogTitle>Create Post</DialogTitle>
-              <DialogDescription>
-                Edit your task post here. Tap on Save when ready.
-              </DialogDescription>
-            </DialogHeader>
-            <div className="grid gap-4 py-4">
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="name" className="text-right">
-                  Project Name
-                </Label>
-                <Input id="name" value="Pedro Duarte" className="col-span-3" />
-              </div>
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="username" className="text-right">
-                  Project Description
-                </Label>
-                <Input id="username" value="@peduarte" className="col-span-3" />
-              </div>
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="username" className="text-right">
-                  Duration
-                </Label>
-                <Input id="username" value="@peduarte" className="col-span-3" />
-              </div>
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="username" className="text-right">
-                  Price
-                </Label>
-                <Input id="username" value="@peduarte" className="col-span-3" />
-              </div>
-            </div>
-            <DialogFooter>
-              <button className="bg-blue-500 text-white rounded px-4 py-2 hover:bg-blue-600">Save changes</button>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
-      </div>
 
-      {/* Dialog Button */}
-      <NewForm />
+        <NewForm />
+      </div>
     </main>
   );
 }
