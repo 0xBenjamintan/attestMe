3d6be64ed0a2fe9fff8ef4e906e3a8c128e36d6c
Terry Lau <terrylau0708@gmail.com>
2023-09-12 01:21:49 +0800

    Update newForm.js

--- components/ui/newForm.js
+++ components/ui/newForm.js
@@ -1,54 +1,39 @@
 import React, { useState } from 'react';
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button"
+import dynamic from 'next/dynamic'
 
 const NewForm = () => {
 
   return (
-    <Card>
-        <div className=" ml spacing-2 w-auto text-center">
-            <h1>Create Task</h1>
+    <Card className="w-[500px]">
+        <div className=" ml spacing-2 w-full">
+            <h1 className='font-bold text-lg mb-6 mt-6 text-center'>Create Task</h1>
 
-                <h2>Task Name: <input className="placeholder=“Task Name"></input> 
-                    <input class="placeholder:text-blue-300 placeholder:italic 
-                         placeholder: w-96 px-5 py-2 rounded-full outline-none"   
-                         placeholder="Your Task Name" 
-                    />
+                <h2 className='mb-4'>Task Name: <input className="placeholder:text-blue-300 placeholder:italic w-96 px-5 py-2 rounded-full outline-none border" placeholder='Task Name'></input> 
+                    
                 </h2>    
 
-                <h2>Details: <input className="placeholder:"></input> 
-                    <input class="placeholder:text-blue-300 placeholder:italic 
-                         placeholder: w-96 px-5 py-2 rounded-full outline-none"   
-                         placeholder="Task Details"
-                    />
+                <h2 className='mb-4'>Details: <input className="placeholder:text-blue-300 placeholder:italic w-96 px-5 py-2 rounded-full outline-none border" placeholder='Task Details'></input> 
+          
                 </h2>
 
-                <h2>Price: <input className="placeholder:"></input> 
-                    <input class="placeholder:text-blue-300 placeholder:italic 
-                         placeholder: w-96 px-5 py-2 rounded-full outline-none"   
-                         placeholder="Task Price" 
-                    />
+                <h2 className='mb-4'>Price: <input className="placeholder:text-blue-300 placeholder:italic w-96 px-5 py-2 rounded-full outline-none border" placeholder='Price'></input> 
+                  
                 </h2>
 
-                <h2>Duration: <input className="placeholder:"></input>
-                    <input class="placeholder:text-blue-300 placeholder:italic 
-                         placeholder: w-96 px-5 py-2 rounded-full outline-none"   
-                         placeholder="Task Duration" 
-                    />
+                <h2 className='mb-4'>Duration: <input className="placeholder:text-blue-300 placeholder:italic w-96 px-5 py-2 rounded-full outline-none border" placeholder='Duration'></input> 
+            
                 </h2>
         </div>
 
         <div className="ml spacing-2 text-center">
-            <Button type="submit" className="btn btn-primary">
+            <Button type="submit" variant="outline" className="btn btn-primary mb-4 mt-4">
                 Create Task
-            </Button>
-
-            <Button className="btn btn-secondary ml-2">
-                Create Post Icon
             </Button>
         </div>    
     </Card>
   );
 };
 
-export default NewForm;
+export default dynamic (() => Promise.resolve(NewForm), {ssr: false} )
